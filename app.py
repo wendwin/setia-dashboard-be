0ec5579a5037099b79228de7329b83a95c30b563
wendwin <wendwin12@gmail.com>
2025-06-11 22:50:20 +0700

    memeperbaiki respon filter per page

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import re
+from dotenv import load_dotenv
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
+load_dotenv() 
+frontend_url = os.environ.get("FRONTEND_URL")
+
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": frontend_url}})
 
 @app.route('/api/data-gmaps', methods=['GET'])
 def dataGmaps():
     data_all = pd.read_csv('./static/data/df_gmaps_reviews.csv')
     reviews = data_all[['name', 'review', 'rating', 'location', 'type_rs']]
+
     count_reviews = len(reviews)
+    count_positive = len(reviews[reviews['rating'].isin([4, 5])])
+    count_negative = len(reviews[reviews['rating'].isin([1, 2, 3])])
 
     location_filter = request.args.get('location', 'all')
     rating_filter = request.args.get('rating', 'all')
@@ -19,31 +27,32 @@ def dataGmaps():
 
     reviews['location'] = reviews['location'].str.lower().str.replace(r'\s+', '_', regex=True)
 
-    # Filter 
+    filtered_reviews = reviews.copy()
+    # filter
     if location_filter != 'all':
-        reviews = reviews[reviews['location'].str.contains(location_filter, case=False)]
+        filtered_reviews = filtered_reviews[filtered_reviews['location'].str.contains(location_filter, case=False)]    
 
     if rating_filter != 'all':
         try:
             rating_filter_value = float(rating_filter)
-            reviews = reviews[reviews['rating'] == rating_filter_value]
+            filtered_reviews = filtered_reviews[filtered_reviews['rating'] == rating_filter_value]
         except ValueError:
-            return jsonify({"error": "Invalid ratingFilter value"}), 400
+            return jsonify({"error": "Invalid ratingFilter value"}), 400       
 
     if type_filter != 'all':
-        reviews = reviews[reviews['type_rs'].str.lower() == type_filter.lower()]
+        filtered_reviews = filtered_reviews[filtered_reviews['type_rs'].str.lower() == type_filter.lower()]    
 
-    # Pagination
+    # Pagination hanya berdasarkan hasil filter
     page = int(request.args.get('page', 1))
     per_page = 10
-    total_reviews = reviews.shape[0]
-    total_pages = (total_reviews // per_page) + (1 if total_reviews % per_page > 0 else 0)
+    filtered_total = len(filtered_reviews)
+    total_pages = (filtered_total // per_page) + (1 if filtered_total % per_page > 0 else 0)       
 
-    paginated_reviews = reviews.iloc[(page - 1) * per_page : page * per_page]
+    paginated_reviews = filtered_reviews.iloc[(page - 1) * per_page : page * per_page]     
 
     start_page = max(page - 5, 1)
     end_page = min(page + 5, total_pages)
-    page_range = list(range(start_page, end_page + 1))
+    page_range = list(range(start_page, end_page + 1))     
 
     return jsonify({
         "reviews": paginated_reviews.to_dict(orient='records'),
@@ -54,9 +63,12 @@ def dataGmaps():
         },
         "filters": {
             "location_filter": location_filter,
-            "rating_filter": rating_filter
+            "rating_filter": rating_filter,
+            "type_filter": type_filter
         },
-        "count": count_reviews
+        "count": count_reviews,           
+        "countPositive": count_positive,  
+        "countNegative": count_negative   
     })
 
 
@@ -90,26 +102,28 @@ def dataGmapsPreprocessing(step):
     reviews['location'] = reviews['location'].str.lower().str.replace(r'\s+', '_', regex=True)
 
     # Filter 
+    filtered_reviews = reviews.copy()
+    # filter
     if location_filter != 'all':
-        reviews = reviews[reviews['location'].str.contains(location_filter, case=False)]
+        filtered_reviews = filtered_reviews[filtered_reviews['location'].str.contains(location_filter, case=False)]    
 
     if rating_filter != 'all':
         try:
             rating_filter_value = float(rating_filter)
-            reviews = reviews[reviews['rating'] == rating_filter_value]
+            filtered_reviews = filtered_reviews[filtered_reviews['rating'] == rating_filter_value]
         except ValueError:
-            return jsonify({"error": "Invalid ratingFilter value"}), 400
+            return jsonify({"error": "Invalid ratingFilter value"}), 400       
 
     if type_filter != 'all':
-        reviews = reviews[reviews['type_rs'].str.lower() == type_filter.lower()]
+        filtered_reviews = filtered_reviews[filtered_reviews['type_rs'].str.lower() == type_filter.lower()]    
 
-    # Pagination
+    # Pagination hanya berdasarkan hasil filter
     page = int(request.args.get('page', 1))
     per_page = 10
-    total_reviews = reviews.shape[0]
-    total_pages = (total_reviews // per_page) + (1 if total_reviews % per_page > 0 else 0)
+    filtered_total = len(filtered_reviews)
+    total_pages = (filtered_total // per_page) + (1 if filtered_total % per_page > 0 else 0)       
 
-    paginated_reviews = reviews.iloc[(page - 1) * per_page : page * per_page]
+    paginated_reviews = filtered_reviews.iloc[(page - 1) * per_page : page * per_page]     
 
     start_page = max(page - 5, 1)
     end_page = min(page + 5, total_pages)
@@ -124,7 +138,8 @@ def dataGmapsPreprocessing(step):
         },
         "filters": {
             "location_filter": location_filter,
-            "rating_filter": rating_filter
+            "rating_filter": rating_filter,
+             "type_filter": type_filter
         },
         "count": count_reviews,
         "step": step_cleaned
@@ -135,32 +150,8 @@ def sentimentAnalysisReview():
     data =  pd.read_csv('./static/data/data_topic_rs_3.csv')
     sentiment = data[['rating', 'type_rs','stemmed_text_done', 'location', 'label', 'predicted_sentiment']]
 
+    count_reviews = len(sentiment)
     sentiment['predicted_sentiment'] = sentiment['predicted_sentiment'].replace({1: 'Positif', -1: 'Negatif'})
-
-    # count_sentiment_positif = sentiment[sentiment['label'].str.contains('Positif', case=False)].shape[0]
-    # count_sentiment_negatif = sentiment[sentiment['label'].str.contains('Negatif', case=False)].shape[0]
-    # count_prediction_positif = sentiment[sentiment['predicted_sentiment'].str.contains('Positif', case=False)].shape[0]
-    # count_prediction_negatif = sentiment[sentiment['predicted_sentiment'].str.contains('Negatif', case=False)].shape[0]
-
-    # Pisahkan ulasan berdasarkan label Positif dan Negatif
-    # positive_reviews = sentiment[sentiment['predicted_sentiment'] == 'Positif']
-    # negative_reviews = sentiment[sentiment['predicted_sentiment'] == 'Negatif']
-
-    # # Membuat model CountVectorizer untuk Positif
-    # vectorizer_pos = CountVectorizer(stop_words='english', max_features=25)
-    # X_pos = vectorizer_pos.fit_transform(positive_reviews['stemmed_text_done'])
-    # words_pos = vectorizer_pos.get_feature_names_out()
-    # frequencies_pos = X_pos.sum(axis=0).A1
-    # word_freq_pos = pd.DataFrame(list(zip(words_pos, frequencies_pos)), columns=['Word', 'Frequency'])
-    # word_freq_pos = word_freq_pos.sort_values(by='Frequency', ascending=False).head(25)
-
-    # # Membuat model CountVectorizer untuk Negatif
-    # vectorizer_neg = CountVectorizer(stop_words='english', max_features=25)
-    # X_neg = vectorizer_neg.fit_transform(negative_reviews['stemmed_text_done'])
-    # words_neg = vectorizer_neg.get_feature_names_out()
-    # frequencies_neg = X_neg.sum(axis=0).A1
-    # word_freq_neg = pd.DataFrame(list(zip(words_neg, frequencies_neg)), columns=['Word', 'Frequency'])
-    # word_freq_neg = word_freq_neg.sort_values(by='Frequency', ascending=False).head(25)
 
     location_filter = request.args.get('location', 'all')
     rating_filter = request.args.get('rating', 'all')
@@ -169,37 +160,32 @@ def sentimentAnalysisReview():
     sentiment['location'] = sentiment['location'].str.lower().str.replace(r'\s+', '_', regex=True)
 
     # Filter 
+    filtered_reviews = sentiment.copy()
+    # filter
     if location_filter != 'all':
-        sentiment = sentiment[sentiment['location'].str.contains(location_filter, case=False)]
+        filtered_reviews = filtered_reviews[filtered_reviews['location'].str.contains(location_filter, case=False)]    
 
     if rating_filter != 'all':
         try:
             rating_filter_value = float(rating_filter)
-            sentiment = sentiment[sentiment['rating'] == rating_filter_value]
+            filtered_reviews = filtered_reviews[filtered_reviews['rating'] == rating_filter_value]
         except ValueError:
-            return jsonify({"error": "Invalid ratingFilter value"}), 400
+            return jsonify({"error": "Invalid ratingFilter value"}), 400       
 
     if type_filter != 'all':
-        sentiment = sentiment[sentiment['type_rs'].str.lower() == type_filter.lower()]
+        filtered_reviews = filtered_reviews[filtered_reviews['type_rs'].str.lower() == type_filter.lower()]    
 
-    # Pagination
+    # Pagination hanya berdasarkan hasil filter
+    page = int(request.args.get('page', 1))
     per_page = 10
-    page = request.args.get('page', 1, type=int)
+    filtered_total = len(filtered_reviews)
+    total_pages = (filtered_total // per_page) + (1 if filtered_total % per_page > 0 else 0)       
 
-    start = (page - 1) * per_page
-    end = start + per_page
+    paginated_reviews = filtered_reviews.iloc[(page - 1) * per_page : page * per_page]     
 
-    paginated_reviews = sentiment.iloc[start:end]
-
-    # Total pages
-    total_reviews = len(sentiment)
-    total_pages = (total_reviews + per_page - 1) // per_page
-
-    # Rentang halaman untuk pagination
-    num_range = 2
-    start_range = max(1, page - num_range)
-    end_range = min(total_pages, page + num_range)
-    page_range = list(range(start_range, end_range + 1))
+    start_page = max(page - 5, 1)
+    end_page = min(page + 5, total_pages)
+    page_range = list(range(start_page, end_page + 1))     
 
     return jsonify({
         "sentiment": paginated_reviews.to_dict(orient='records'),
@@ -210,9 +196,10 @@ def sentimentAnalysisReview():
         },
         "filters": {
             "location_filter": location_filter,
-            "rating_filter": rating_filter
+            "rating_filter": rating_filter,
+             "type_filter": type_filter
         },
-
+         "count": count_reviews,  
         # "count_sentiment_positif": count_sentiment_positif,
         # "count_sentiment_negatif": count_sentiment_negatif,
         # "count_prediction_positif": count_prediction_positif,
